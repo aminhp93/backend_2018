f5698895562821bf1c6b8eecb57a6d267380dde7
Minh Pham <minhpham@Minhs-MBP.net.fpt>
2018-10-18 21:39:06 +0700

    Add return json

--- posts/views.py
+++ posts/views.py
@@ -3,9 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from django.http import JsonResponse
 import datetime
 
 def test(request):
     now = datetime.datetime.now()
     html = "<html><body>It is now %s.</body></html>" % now
-    return HttpResponse(html)
+    # return HttpResponse(html)
+    return JsonResponse({'foo': 'bar'})
