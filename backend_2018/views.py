03c3a8d19a09314a6b8e52204e1fd11f5f3c029b
Minh Pham <minh.pham@quant-edge.com>
2019-05-06 12:22:42 +0700

    Remove socketio

--- backend_2018/views.py
+++ backend_2018/views.py
@@ -1,24 +1,24 @@
 from django.http import JsonResponse
 import requests
 import json
-import socketio
-sio = socketio.Server()
+# import socketio
+# sio = socketio.Server()
 
 
-@sio.on('connect', namespace='/chat')
-def connect(sid, environ):
-    print("connect ", sid)
+# @sio.on('connect', namespace='/chat')
+# def connect(sid, environ):
+#     print("connect ", sid)
 
 
-@sio.on('chat message', namespace='/chat')
-def message(sid, data):
-    print("message ", data)
-    sio.emit('reply', room=sid)
+# @sio.on('chat message', namespace='/chat')
+# def message(sid, data):
+#     print("message ", data)
+#     sio.emit('reply', room=sid)
 
 
-@sio.on('disconnect', namespace='/chat')
-def disconnect(sid):
-    print('disconnect ', sid)
+# @sio.on('disconnect', namespace='/chat')
+# def disconnect(sid):
+#     print('disconnect ', sid)
 
 
 def home(request):
