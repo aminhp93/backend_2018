a5e5129d5c0e741800fe5404b8eaf9ed93beb308
MINH PHAM <minhpn.org.ec@gmail.com>
2019-08-25 14:48:14 +0700

    Fix update db

--- core/views.py
+++ core/views.py
@@ -8,17 +8,25 @@ from .models import Config
 
 @csrf_exempt
 def update(request):
+    # if datetime.now().strftime('%a') == 'Sat' or datetime.now().strftime('%a') == 'Sun':
+    #     return JsonResponse({'data': 'Today is sat or sun'})
     if request.method == 'POST':
         body = json.loads(request.body.decode('utf-8'))
         if not 'date' in body:
             return JsonResponse({'data': 'error'})
+        
         configs = Config.objects.filter(key='LAST_UPDATED_TIME')
         if len(configs) > 0:
             last_updated_time = configs[0]
             last_updated_time.value = body['date']
             last_updated_time.save()
-            return JsonResponse({'data': 'LAST_UPDATED_TIME success'})
-        return JsonResponse({'data': 'error'})
+            return JsonResponse({'data': 'Updated LAST_UPDATED_TIME success'})
+        else:
+            last_updated_time = Config()
+            last_updated_time.key = 'LAST_UPDATED_TIME'
+            last_updated_time.value = body['date']
+            last_updated_time.save()
+            return JsonResponse({'data': 'Created LAST_UPDATED_TIME success'})
     return JsonResponse({'data': 'error'})
 
 def get_last_updated_time(request):
